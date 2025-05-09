97e5cc272e863fb3c93226856b373639d42a18a1
Sid Price <sid@sidprice.com>
2025-05-09 14:22:52 -0600

    ctxlink: Clang formatting

--- src/tasks/task_server.cpp
+++ src/tasks/task_server.cpp
@@ -4,9 +4,9 @@
  * @brief Socket based server for ctxLink ESP32 Wi-Fi adapter
  * @version 0.1
  * @date 2025-03-24
- * 
+ *
  * @copyright Copyright Sid Price (c) 2025
- * 
+ *
  */
 
 #include <Arduino.h>
@@ -18,28 +18,30 @@
 #include "task_spi_comms.h"
 #include "protocol.h"
 
+#include "debug.h"
+
 #include "ctxlink.h"
 
-char net_input_buffer[1024] ;   // Data received from network
-char from_ctxlink_buffer[1024] ;
+char net_input_buffer[2048]; // Data received from network
 
-QueueHandle_t gdb_server_queue ;
+QueueHandle_t gdb_server_queue;
 
-constexpr uint32_t gdb_server_queue_length = 4 ;
+constexpr uint32_t gdb_server_queue_length = 16;
 
 /**
  * @brief Task to handle the GDB Wi-Fi server
- * 
- * @param pvParameters 
+ *
+ * @param pvParameters
  */
-void task_wifi_server(void *pvParameters) {
+void task_wifi_server(void *pvParameters)
+{
     int server_fd, client_fd;
     struct sockaddr_in server_addr, client_addr;
 
-    gdb_server_queue = xQueueCreate(gdb_server_queue_length, sizeof(uint8_t *)) ; // Create the queue for the GDB server task
+    gdb_server_queue = xQueueCreate(gdb_server_queue_length, sizeof(uint8_t *)); // Create the queue for the GDB server task
     socklen_t addr_len = sizeof(client_addr);
 
-    in_port_t port = (in_port_t)((uint32_t)pvParameters);   // Recover the port number for this task
+    in_port_t port = (in_port_t)((uint32_t)pvParameters); // Recover the port number for this task
 
     // Create socket
     server_fd = socket(AF_INET, SOCK_STREAM, 0);
@@ -50,87 +52,110 @@ void task_wifi_server(void *pvParameters) {
     server_addr.sin_port = htons(port);
 
     // Bind socket to address
-    if ( bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
-        MONITOR(print("Socket bind failed -> ")) ; MONITOR(println(errno)) ;
+    if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
+    {
+        MONITOR(print("Socket bind failed -> "));
+        MONITOR(println(errno));
         return;
     }
 
     // Listen for incoming connections
-    if (listen(server_fd, 5) < 0) { // TODO: Test for single client operation, set to 1 or 0?
-        MONITOR(print("Socket listen failed -> ")) ; MONITOR(println(errno)) ;
+    if (listen(server_fd, 5) < 0)
+    { // TODO: Test for single client operation, set to 1 or 0?
+        MONITOR(print("Socket listen failed -> "));
+        MONITOR(println(errno));
         return;
     }
 
     //
     // Just halt here for now
     //
-    MONITOR(println("Server task started")) ;
+    MONITOR(println("Server task started"));
     //
     // Main server loop
     //
-    while(true) {
+    while (true)
+    {
         // Accept incoming connections
-        MONITOR(print("Server listening on port ")) ; MONITOR(println(port)) ;
-        while (true) {
+        MONITOR(print("Server listening on port "));
+        MONITOR(println(port));
+        while (true)
+        {
             client_fd = accept(server_fd, (struct sockaddr *)&client_addr, &addr_len);
-            if (client_fd < 0) {
-                MONITOR(print("Socket accept failed")); MONITOR(println(errno)) ;
+            if (client_fd < 0)
+            {
+                MONITOR(print("Socket accept failed"));
+                MONITOR(println(errno));
                 return;
-            } 
+            }
             MONITOR(println("Client connected"));
             // Set the client socket to non-blocking mode
             int flags = fcntl(client_fd, F_GETFL, 0);
+            // int flags = 1 ;
             fcntl(client_fd, F_SETFL, flags | O_NONBLOCK);
             flags = 1;
-            setsockopt(client_fd, IPPROTO_TCP, TCP_NODELAY, &flags, sizeof(flags)) ;
+            setsockopt(client_fd, IPPROTO_TCP, TCP_NODELAY, &flags, sizeof(flags));
             //
             //  Server data handling loop
             //
-            while(true) {
-                BaseType_t result ;
-                uint8_t *message ;
+            while (true)
+            {
+                BaseType_t result;
+                uint8_t *message;
                 //
                 // Check for packets from SPI task
                 //
-                result = xQueueReceive(gdb_server_queue, &message, 0) ;  // Do not block here
-                if (result == pdTRUE) {
-                    size_t packet_size ;
-                    protocol_packet_type_e packet_type ;
-                    uint8_t *packet_data ;
-                    MONITOR(println("Packet from SPI")) ;
-                    protocol_split(message, &packet_size, &packet_type, &packet_data) ;
-                    MONITOR(print("packet_type: ")) ; MONITOR(println(packet_type)) ;
-                    MONITOR(print("packet_size: ")) ; MONITOR(println(packet_size)) ; 
-                    MONITOR(print("message: ")) ; MONITOR(println((char *)packet_data)) ;
-                    Serial.println("packet to gdb") ;
-                    send(client_fd, packet_data, packet_size, 0); // Send the data to the client
+                result = xQueueReceive(gdb_server_queue, &message, 0); // Do not block here
+                if (result == pdTRUE)
+                {
+                    size_t bytes_sent;
+                    size_t packet_size;
+                    protocol_packet_type_e packet_type;
+                    uint8_t *packet_data;
+                    protocol_split(message, &packet_size, &packet_type, &packet_data);
+                    // MONITOR(println((char *)packet_data));
+                    send(client_fd, packet_data, packet_size, 0);
                 }
                 int bytes_received = read(client_fd, &net_input_buffer, sizeof(net_input_buffer));
-                if (bytes_received > 0 ) {
+                if (bytes_received > 0)
+                {
+                    size_t packed_size;
                     // MONITOR(print("Bytes received: ")) ; MONITOR(println(bytes_received)) ;
-                    // for (int i = 0; i < bytes_received; i++) {
-                    //     MONITOR(print(net_input_buffer[i])) ;
-                    // }
-                    // MONITOR(println()) ;
-                    //
                     // Send input to the SPI task for forwarding to ctxLink
                     //
-                    package_data((uint8_t *)net_input_buffer, bytes_received, PROTOCOL_PACKET_TYPE_TO_CTXLINK, sizeof(net_input_buffer)) ; // Package the data for ctxLink
-                    uint8_t *input_message = (uint8_t *)net_input_buffer ; // Cast the buffer to a uint8_t pointer
-                    xQueueSend(spi_comms_queue, &input_message, 0) ; // Send the data to the SPI task
-                } else if (bytes_received == 0) {
+                    packed_size = package_data((uint8_t *)net_input_buffer, bytes_received, PROTOCOL_PACKET_TYPE_FROM_GDB);
+                    // MONITOR(print("Bytes received: "));
+                    // MONITOR(println(bytes_received));
+                    // for (int i = 0; i < packed_size; i++)
+                    // {
+                    //     MONITOR(printf("%02x ", net_input_buffer[i]));
+                    // }
+                    // MONITOR(println());
+                    uint8_t *input_message = (uint8_t *)net_input_buffer;
+                    TOGGLE_PIN(PINA) ;
+                    xQueueSend(spi_comms_queue, &input_message, 0);
+                }
+                else if (bytes_received == 0)
+                {
                     MONITOR(println("Client disconnected"));
                     close(client_fd);
                     break;
-                } else if (errno == EAGAIN || errno == EWOULDBLOCK) {
+                }
+                else if (errno == EAGAIN || errno == EWOULDBLOCK)
+                {
                     // No data available, continue the loop
-                    vTaskDelay(10 / portTICK_PERIOD_MS); // Yield to other tasks
-                }  else if (errno == ECONNRESET) {
+                    // vTaskDelay(10 / portTICK_PERIOD_MS); // Yield to other tasks
+                }
+                else if (errno == ECONNRESET)
+                {
                     MONITOR(println("Client disconnected abruptly (ECONNRESET)"));
                     close(client_fd);
                     break;
-                } else {
-                    MONITOR(print("Socket read failed: ")); MONITOR(println(errno));
+                }
+                else
+                {
+                    MONITOR(print("Socket read failed: "));
+                    MONITOR(println(errno));
                     close(client_fd);
                     break;
                 }
@@ -139,6 +164,5 @@ void task_wifi_server(void *pvParameters) {
                 //
             }
         }
-            
     }
 }
