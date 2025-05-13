905dd0410d94022798e86921c6d6c38089653c98
Sid Price <sid@sidprice.com>
2025-05-13 11:38:21 -0600

    task_spi: Clean up debug comments

--- src/tasks/task_spi_comms.cpp
+++ src/tasks/task_spi_comms.cpp
@@ -103,14 +103,8 @@ void task_spi_comms(void *pvParameters)
     static uint8_t *message;
     spi_comms_queue = xQueueCreate(spi_comms_queue_length, sizeof(uint8_t *)); // Create the queue for the SPI task
 
-    //
-    // Create a transaction, ready to receive data from the master
-    //
-    // spi_create_pending_transaction(NULL, get_next_spi_buffer(), false) ; // This is a pending rx transaction
-
     while (true)
     {
-        // MONITOR(println("SPI Task Loop Start")) ;
         // Wait for a message from the other tasks or spi driver
         xQueueReceive(spi_comms_queue, &message, portMAX_DELAY);
         //
@@ -121,10 +115,7 @@ void task_spi_comms(void *pvParameters)
         protocol_packet_type_e packet_type;
         uint8_t *packet_data;
         packet_size = protocol_split(message, &data_length, &packet_type, &packet_data);
-        // Serial.print("packet_type: ") ; Serial.println(packet_type) ;
-        // Serial.print("data_length: ") ; Serial.println(data_length) ;
-        // Serial.print("message: ") ; Serial.println((char *)packet_data) ;
-        //
+
         switch (packet_type)
         {
         case PROTOCOL_PACKET_TYPE_EMPTY:
@@ -135,31 +126,22 @@ void task_spi_comms(void *pvParameters)
         case PROTOCOL_PACKET_TYPE_TO_GDB:
         {
             //
-            // Print the message for now
-            //
-            // MONITOR(printf("Packet type: %02X\r\n", packet_type)) ;
-            //            MONITOR(println((char*)packet_data)) ;
-            //
             // Send the packet to the server task
             //
             // TODO Need to check if there is a client attached to GDB server
             //
-            // Serial.println("packet to gdb server") ;
             xQueueSend(gdb_server_queue, &message, portMAX_DELAY); // Send the message to the gdb server task
             break;
         }
+        //
+        // The following cases fall-through to common code
+        // to send the received message to ctxLink
+        //
         case PROTOCOL_PACKET_TYPE_NETWORK_INFO:
-            MONITOR(println(packet_type));
-            break ;
         case PROTOCOL_PACKET_TYPE_FROM_GDB:
         {
-            MONITOR(print("Bytes received: ")) ; MONITOR(println(packet_size)) ;
-            // for (int i = 0; i < packet_size; i++) {
-            //     MONITOR(printf("%02x ",message[i])) ;
-            // }
-            // MONITOR(println()) ;
             spi_save_tx_transaction_buffer(message); // Save the transaction buffer for SPI driver
-            TOGGLE_PIN(PINB) ;
+            TOGGLE_PIN(PINB);
             break;
         }
         default:
