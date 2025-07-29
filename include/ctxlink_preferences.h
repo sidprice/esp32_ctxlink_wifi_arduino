4189da783893459e83b151e80a819e7926dd5410
Sid Price <sid@sidprice.com>
2025-07-29 09:47:32 -0600

    wifi_task: Add Wi-Fi preferences management

---
+++ include/ctxlink_preferences.h
@@ -0,0 +1,21 @@
+/**
+ * @file ctxlink_preferences.cpp
+ * @author Sid Price (sid@sidprice.com)
+ * @brief Preferences management for ctxLink module
+ * @version 0.1
+ * @date 2025-07-27
+ *
+ * @copyright Copyright Sid Price (c) 2025
+ *
+ * This module provides support for ctxLink preferences
+ */
+
+#ifndef CTXLINK_PREFERENCES_H
+#define CTXLINK_PREFERENCES_H
+
+#include <Arduino.h>
+
+void preferences_init(void) ;
+size_t preferences_get_wifi_parameters(char *ssid, char *password) ;
+void preferences_save_wifi_parameters(char *ssid, char *password) ;
+#endif // CTXLINK_PREFERENCES_H
