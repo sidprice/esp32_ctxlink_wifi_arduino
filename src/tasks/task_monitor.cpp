912656f2c7f01c36d93253fafd2147ca677811b8
Sid Price <sid@sidprice.com>
2025-08-02 13:46:37 -0600

    Expand logging functionality to implement multi-threaded monitor output

---
+++ src/tasks/task_monitor.cpp
@@ -0,0 +1,39 @@
+/**
+ * @file task_monitor.cpp
+ * @author Sid Price (sid@sidprice.com)
+ * @brief
+ * @version 0.1
+ * @date 2025-04-20
+ *
+ * @copyright Copyright Sid Price (c) 2025
+ *
+ */
+
+#include <Arduino.h>
+#include "serial_control.h"
+#include "task_monitor.h"
+
+/**
+ * @brief The queue handle for the monitor output task  
+ * 
+ */
+QueueHandle_t task_monitor_queue;
+
+/**
+ * @brief Task to output system monitor messages
+ *
+ * @param pvParameters Unused parameter
+ */
+void task_monitor(void *pvParameters) {
+    (void)pvParameters;
+    //
+    // Create the input queue for the task
+    //
+    task_monitor_queue = xQueueCreate(MONITOR_OUTPUT_QUEUE_DEPTH, sizeof(monitor_output_message_t));
+    while (1) {
+        monitor_output_message_t message;
+        if (xQueueReceive(task_monitor_queue, &message, portMAX_DELAY) == pdTRUE) {
+            Serial.print(message.message);
+        }
+    }
+}
---
+++ src/tasks/task_monitor.h
@@ -0,0 +1,50 @@
+/**
+ * @file task_monitor.h
+ * @author Sid Price (sid@sidprice.com)
+ * @brief
+ * @version 0.1
+ * @date 2025-04-20
+ *
+ * @copyright Copyright Sid Price (c) 2025
+ *
+ */
+
+#ifndef TASK_MONITOR_H
+#define TASK_MONITOR_H
+
+#include <Arduino.h>
+#include "serial_control.h"
+
+/**
+ * @brief Define the queue handle for the monitor output task
+ * 
+ */
+extern QueueHandle_t task_monitor_queue;
+
+/**
+ * @brief Define the maximum length of a monitor output message
+ * 
+ */
+#define MONITOR_OUTPUT_MAX_LENGTH 80
+/**
+ * @brief Define the depth of the monitor output task message queue
+ * 
+ */
+#define MONITOR_OUTPUT_QUEUE_DEPTH 10
+
+/**
+ * @brief Define the message structure for the monitor output task
+ * 
+ */
+typedef struct {
+    char message[MONITOR_OUTPUT_MAX_LENGTH];
+} monitor_output_message_t;
+
+/**
+ * @brief The task that schedules monitor output messages
+ * 
+ * @param pvParameters 
+ */
+void task_monitor(void *pvParameters);
+
+#endif // TASK_MONITOR_H
